d400059d9b07357e499180072fbf079ce291fcfd
s3804220 <s3804220@rmit.edu.vn>
2020-05-18 10:40:23 +0700

    Tut W9

---
+++ ce/Tut W9/tools.php
@@ -0,0 +1,3 @@
+$moviesObject = ['ACT' => ['title' => 'Avengers: Endgame', 'rating' => 'R', 'description' => '<p>With the help of remaining allies', 'screening'=>['WED' => 'T21','THU' => 'T21']], 
+                'RMC' => ['title' => 'Happy Wedding', 'rating' => 'M', 'description' => '<p>lorem ipsum'], 'screening'=>['WED' => 'T21','THU' => 'T21']
+                    ];
