4cf4fba1130768008c92290101580defb480720a
s3804220 <s3804220@rmit.edu.vn>
2020-05-20 13:48:43 +0700

    added receipt-style.css and initial tax invoice

--- a4/receipt.php
+++ a4/receipt.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Receipt</title>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="receipt-style.css">
     <?php include 'tools.php';?>
 </head>
 <body>
@@ -19,5 +20,22 @@
     }
     fclose($fp);
     ?>
+
+    <h1 style="text-align: center;">Tax Invoice </h1>
+
+    <div class="page">
+        <h1 style="text-align: center;">CINEMAX Entertainment Inc. </h1>
+        <h1 style="text-align: center;">ABN number: 00 123 456 789  </h1>
+        <br><br>
+        <h2>Customer Information</h2>
+        <p>Name: <?php echo $_SESSION['cart']['cust']['name']?></p>
+        <p>Email <?php echo $_SESSION['cart']['cust']['email']?></p>
+        <p>Mobile: <?php echo $_SESSION['cart']['cust']['mobile']?></p>
+        <br>
+        <h2>Movie Information</h2>
+        <p>Movie date: <?php echo $_SESSION['cart']['movie']['day']?></p>
+        <p>Movie hour code: <?php echo $_SESSION['cart']['movie']['hour']?></p>
+        <p>Movie ID: <?php echo $_SESSION['cart']['movie']['id']?></p>
+    </div>
 </body>
 </html>
